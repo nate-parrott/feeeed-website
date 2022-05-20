00def0e7dbd5d051cb8f51ebce240ef4d87b4fbd
nate <nateparro2t@gmail.com>
2022-05-20 00:46:45 -0400

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 import Base from '../components/base'
 import Header from '../components/header'
@@ -16,9 +17,32 @@ const Home: NextPage = () => {
 
       <Base>
         <Header />
-        
+        <div className={styles.screenshots}>
+          <Image src="/s1.png" width="512" height="1109" />
+          <Image src="/s2.png" width="512" height="1109" />
+          <Image src="/s3.png" width="512" height="1109" />
+        </div>
+
         <h1>Build your own news feed</h1>
-        <h3>Follow any website</h3>
+
+        <p>
+        you spend hours scrolling feeds all day. shouldn't YOU be in control of what you see, rather than some algorithm?
+        </p>
+        
+        <p>
+          with feeeed, you can build your own news feed, on your own terms:
+        </p>
+        <ul>
+        <li> subscribe to any website, using RSS, Twitter or just seeing a copy of their website in your feed</li>
+        <li>see local weather, or flashbacks from your photo library</li>
+        <li>subscribe to any subreddit, or read newsletters from your gmail inbox</li>
+        <li>add your own custom reminders, or flashcards, or see your step count for the day</li>
+        </ul>
+        
+        <p>
+          take back your news feed ~ scroll on your own terms :)
+        </p>
+
         <footer className={styles.footer}>
           <Link href='/privacy'>Privacy</Link>
           <Link href='/terms'>Terms</Link>
